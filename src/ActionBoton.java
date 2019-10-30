e80591a9d79125342db7a4f5fce8a0af27602243
Sergio García <sergiogarcia2893@gmail.com>
2019-10-30 19:41:22 +0100

    Quitamos imports innecesarios.

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -1,12 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 
 import java.io.FileInputStream;
